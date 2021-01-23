5415c64713d4dbf2ad5b80a04c9f5a7550a57a74
Ananda Ryanizar <anandar.ryanizar@gmail.com>
2021-01-23 15:33:14 +0700

    init commit

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -4,4 +4,26 @@ import com.workshop.Config
 
 class Pipeline {
     // Set pipeline struct
+    String repository_name
+    String branch_name
+    String git_user
+    String docker_user
+    String app_port
+    String pr_num
+
+    Pipeline(
+        String repository_name,
+        String branch_name,
+        String git_user,
+        String docker_user,
+        String app_port,
+        String pr_num
+    ){
+        this.repository_name = repository_name
+        this.branch_name = branch_name
+        this.git_user = git_user
+        this.docker_user = docker_user
+        this.app_port = app_port
+        this.pr_num = pr_num
+    }
 }
