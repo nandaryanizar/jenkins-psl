a1eff5ddba05d5b4639c2dd14f609ae86a50cad9
Ananda Ryanizar <anandar.ryanizar@gmail.com>
2021-01-23 16:42:12 +0700

    add cleanup docker image to postbuild stage

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -70,6 +70,10 @@ def main(script) {
         stage('Service Healthcheck') {
             spostdeploy.healthcheck(p)
         }
+
+        stage('Delete Old Image'){
+           spostdeploy.deleteOldImage(p)
+        }
     }
 }
 
